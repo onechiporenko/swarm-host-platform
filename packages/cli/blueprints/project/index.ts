57347543daa05685cb4a9cfe572bcaabb1d5d956
Oleg Nechiporenko <oo.nechiporenko@gmail.com>
2022-12-26 19:58:59 +0200

    Add some new features

--- packages/cli/blueprints/project/index.ts
+++ packages/cli/blueprints/project/index.ts
@@ -1,7 +1,9 @@
 import server from './app/server';
+import serverSetup from './server-setup';
 
 server.addRoutesFromDir(`${__dirname}/app/routes`).then(() => {
   server.addFactoriesFromDir(`${__dirname}/app/factories`).then(() => {
+    serverSetup(server);
     server.startServer();
   });
 });
---
+++ packages/cli/blueprints/project/server-setup.ts
@@ -0,0 +1,7 @@
+import { Server } from '@swarm-host/server';
+
+const serverSetup = (server: Server): void => {
+  // do anything with server
+};
+
+export default serverSetup;
